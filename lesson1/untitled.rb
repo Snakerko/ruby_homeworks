b622f0badfeab29b983c3b70ffb815205fba2e1c
Snakerko <30870896+Snakerko@users.noreply.github.com>
2018-04-05 01:38:34 +0300

    Delete untitled.rb

--- lesson1/untitled.rb
+++
@@ -1,8 +0,0 @@
-uname = gets.chomp.capitalize
-length = gets.chomp.to_i
-
-def optimal_weight
-  weight = length - 110
-  weight > 0 ? puts "#{uname} ваш оптимальный вес составляет #{weight} кг." :
-  puts "Ваш вес уже оптимальный"
-end
